418beaccc7ad67fe0ae16b079ab43e679d7e5e06
kaurilves <kaurilves@gmail.com>
2022-11-08 13:30:37 +0200

    Vendor feature done

--- db_scripts/db_script.sql
+++ db_scripts/db_script.sql
@@ -1,11 +1,12 @@
 -- MySQL Workbench Forward Engineering
 
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `asset_management` ;
+DROP SCHEMA IF EXISTS `asset_management`;
 CREATE SCHEMA IF NOT EXISTS `asset_management` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `asset_management` ;
 
@@ -23,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `asset_management`.`vendors` (
   `description` VARCHAR(255) NULL,
   `rate` DECIMAL(13,2) NULL,
   `date_created` DATE NOT NULL,
+  `userCompanyId` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `idtable1_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `company_name_UNIQUE` (`company_name` ASC) VISIBLE)
